11b9402a070ded4e0837b0e09aaa5ad7c782032d
Gabriel Lima <gabrolima@gmail.com>
2024-02-24 16:26:48 -0600

    cambio para la nueva app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'gt.asms.beemoree',
+  appId: 'com.farasi.beemore',
   appName: 'BeeMore',
   webDir: 'www',
   server: {
